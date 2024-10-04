70aaac0d0ae6ecd09fbfb44e7cca954f2b44c195
Pranav Dasan <62214486+pranavdasan@users.noreply.github.com>
2024-10-04 15:53:19 +0000

    Completed LinkedList Lab code

--- LinkedList Lab/ListFunHouse.java
+++ LinkedList Lab/ListFunHouse.java
@@ -9,47 +9,79 @@
 
 import static java.lang.System.*;
 
-public class ListFunHouse
-{
-	//this method will print the entire list on the screen
-   public static void print(ListNode list)
-   {
-   }		
-	
-	//this method will return the number of nodes present in list
-	public static int nodeCount(ListNode list)
-	{
-   	int count=0;
+public class ListFunHouse {
+	// this method will print the entire list on the screen
+	public static void print(ListNode list) {
+				//Prints all the nodes but the last one
+		while(list.getNext() != null) {
+			System.out.print("["+list.getValue()+"] -> ");
+			list = list.getNext();
+		}
+
+		// Prints the last node (is out of the loop for formating reasons)
+		System.out.print("["+list.getValue()+"]");
+	}
+
+	// this method will return the number of nodes present in list
+	public static int nodeCount(ListNode list) {
+		int count = 0;
+
+		while(list != null) {
+			count++;
+			list = list.getNext();
+		}
+
 		return count;
 	}
-		
-	//this method will create a new node with the same value as the first node and add this
-	//new node to the list.  Once finished, the first node will occur twice.
-	public static void doubleFirst(ListNode list)
-	{
+
+	// this method will create a new node with the same value as the first node and
+	// add this
+	// new node to the list. Once finished, the first node will occur twice.
+	public static void doubleFirst(ListNode list) {
+		list.setNext(new ListNode(list.getValue(), list.getNext()));
 	}
 
-	//this method will create a new node with the same value as the last node and add this
-	//new node at the end.  Once finished, the last node will occur twice.
-	public static void doubleLast(ListNode list)
-	{
-   	ListNode prev=null;
-	}
-		
-	//method skipEveryOther will remove every other node
-	public static void skipEveryOther(ListNode list)
-	{
+	// this method will create a new node with the same value as the last node and
+	// add this
+	// new node at the end. Once finished, the last node will occur twice.
+	public static void doubleLast(ListNode list) {
+		Comparable lastValue = null;
+
+		// Get the last value
+		while (list.getNext() != null) {
+			list = list.getNext();
+		}
+
+		lastValue = list.getValue();
+
+		list.setNext(new ListNode(lastValue, null));
 	}
 
-	//this method will set the value of every xth node in the list
-	public static void setXthNode(ListNode list, int x, Comparable value)
-	{
-		int count=1;
-	}	
+	// method skipEveryOther will remove every other node
+	public static void skipEveryOther(ListNode list) {
+	}
 
-	//this method will remove every xth node in the list
-	public static void removeXthNode(ListNode list, int x)
-	{
-		int count=1;
-	}		
+	// this method will set the value of every xth node in the list
+	public static void setXthNode(ListNode list, int x, Comparable value) {
+		int count = 1;
+
+		while(list != null) {
+			if (count % x == 0) {
+				list.setValue(value);
+			}
+
+			count++;
+			list = list.getNext();
+		}
+	}
+
+	// this method will remove every xth node in the list
+	public static void removeXthNode(ListNode list, int x) {
+		while(list.getNext().getNext() != null) {
+			list.setNext(list.getNext().getNext());
+			list = list.getNext();
+		}
+
+		list.setNext(null);
+	}
 }
--- LinkedList Lab/ListFunHouseRunner.java
+++ LinkedList Lab/ListFunHouseRunner.java
@@ -6,49 +6,47 @@
 import java.util.*;
 import static java.lang.System.*;
 
-public class ListFunHouseRunner
-{
-	public static void main ( String[] args )
-	{
+public class ListFunHouseRunner {
+	public static void main(String[] args) {
 		ListNode z = new ListNode("go",
-			 			new ListNode("on",
-		      		new ListNode("at",
-		       		new ListNode("34",
-			 			new ListNode("2.1",
-		      		new ListNode("-a-2-1",
-		       		new ListNode("up",		       		
-			 			new ListNode("over",null))))))));
-			 			
-		out.println("Lab15b Test Code\n\n");	
-		
-		out.println("Original list values\n");	
+				new ListNode("on",
+						new ListNode("at",
+								new ListNode("34",
+										new ListNode("2.1",
+												new ListNode("-a-2-1",
+														new ListNode("up",
+																new ListNode("over", null))))))));
+
+		out.println("Lab15b Test Code\n\n");
+
+		out.println("Original list values\n");
 		ListFunHouse.print(z);
 		out.println("\n");
-		
+
 		out.println("num nodes = " + ListFunHouse.nodeCount(z));
 
-		out.println("\nList values after calling nodeCount\n");	
+		out.println("\nList values after calling nodeCount");
 		ListFunHouse.print(z);
-		out.println();		
+		out.println();
 
-		ListFunHouse.doubleFirst(z);		
-		out.println("\nList values after calling doubleFirst\n");							
+		ListFunHouse.doubleFirst(z);
+		out.println("\nList values after calling doubleFirst");
 		ListFunHouse.print(z);
-		out.println();	 
+		out.println();
 
-		ListFunHouse.doubleLast(z);		
-		out.println("\nList values after calling doubleLast\n");							
+		ListFunHouse.doubleLast(z);
+		out.println("\nList values after calling doubleLast");
 		ListFunHouse.print(z);
-		out.println();				
-				
-		ListFunHouse.removeXthNode(z,2);		
-		out.println("\nList values after calling removeXthNode(2)\n");					
+		out.println();
+
+		ListFunHouse.removeXthNode(z, 2);
+		out.println("\nList values after calling removeXthNode(2)");
 		ListFunHouse.print(z);
-		out.println();			
-				
-		ListFunHouse.setXthNode(z,2,"one");		
-		out.println("\nList values after calling setXthNode(2,one)\n");										
+		out.println();
+
+		ListFunHouse.setXthNode(z, 2, "one");
+		out.println("\nList values after calling setXthNode(2,one)");
 		ListFunHouse.print(z);
-		out.println();				
+		out.println();
 	}
 }
